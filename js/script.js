c46a4c970bda15fe1b9e219ab1ce5b60d50b910a
ascottishpenguin <crazyrandomscottishpenguin@hotmail.com>
2019-05-14 23:38:13 +0100

    Javascript formula complete, CSS required

--- js/script.js
+++ js/script.js
@@ -1,6 +1,28 @@
-function playRound(playerSelection, computerSelection) {
-	// your code here!
+//document.getElementById("rock").addEventListener("click")
+let scorePlayer = 0;
+let scoreComputer = 0;
+
+function playRound(playerSelection) {
+    const computerSelectionType = ['rock', 'paper', 'scissors'];
+    const i = Math.floor(Math.random() * 3)
+    let computerSelection = computerSelectionType[i]
+    if (playerSelection == 'rock' && computerSelection == 'rock' || playerSelection == 'paper' && computerSelection == 'paper' || playerSelection == 'scissors' && computerSelection == 'scissors') {
+        document.getElementById("results").textContent = "Computer chose "+computerSelection+"! It's a draw!"
+    }
+    else if (playerSelection == 'rock' &&  computerSelection == 'paper' || playerSelection == 'paper' && computerSelection == 'scissors' || playerSelection == 'scissors' && computerSelection == 'rock')  {
+        document.getElementById("results").textContent = "Computer chose "+computerSelection+"! You Lose!"
+        scoreComputer += 1
+        document.getElementById("computerscore").textContent = scoreComputer
+    } 
+    else if (playerSelection == 'rock' &&  computerSelection == 'scissors' || playerSelection == 'paper' && computerSelection == 'rock' || playerSelection == 'scissors' && computerSelection == 'paper')  {
+        document.getElementById("results").textContent = "Computer chose "+computerSelection+"! You Lose!"
+        scorePlayer += 1
+        document.getElementById("playerscore").textContent = scorePlayer
+    } 
+        
 }
+
+
 
 const playerSelection = 'rock'
 const computerSelection = computerPlay()
